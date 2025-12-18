219819ce8097974d89494ecabeda670a68f8959c
Ashish Roy <58362174+RoyAshish80@users.noreply.github.com>
2025-12-19 01:32:15 +0530

    Configured prettier

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,16 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-
-  res.send('Hello, World!');
-  
+   res.send('Hello, World!');
 });
 app.get('/api/hello', (req: Request, res: Response) => {
-
-  res.json({message: 'Hello, World!'});
-  
+   res.json({ message: 'Hello, World!' });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+   console.log(`Server is running on http://localhost:${PORT}`);
 });
