7aefec6860e2e4eb56ae2f559bce3c87b5159934
Ashish Roy <58362174+RoyAshish80@users.noreply.github.com>
2025-12-31 02:44:52 +0530

    Introducing repository

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import OpenAI from 'openai';
+import z, { set } from 'zod';
+import { conversationRepository } from './repositories/conversation.repository';
 
 dotenv.config();
 
+const client = new OpenAI({
+   apiKey: process.env.OPENAI_API_KEY1,
+});
+
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
@@ -14,6 +22,43 @@ app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello, World!' });
 });
 
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is required')
+      .max(1000, 'Prompt is too long, maximum of 1000 characters allowed.'),
+   conversationId: z.uuid(),
+});
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+   const parseResult = chatSchema.safeParse(req.body);
+   if (!parseResult.success) {
+      return res.status(400).json(parseResult.error.format());
+      return;
+   }
+
+   try {
+      const { prompt, conversationId } = req.body;
+
+      const response = await client.responses.create({
+         model: 'gpt-4o-mini',
+         input: prompt,
+         temperature: 0.2,
+         max_output_tokens: 100,
+         previous_response_id:
+            conversationRepository.getLastResponseId(conversationId),
+      });
+
+      conversationRepository.setLastResponseId(conversationId, response.id);
+
+      res.json({ message: response.output_text });
+   } catch (error) {
+      res.status(500).json({
+         error: 'An error occurred while processing your request.',
+      });
+   }
+});
 app.listen(PORT, () => {
    console.log(`Server is running on http://localhost:${PORT}`);
 });
